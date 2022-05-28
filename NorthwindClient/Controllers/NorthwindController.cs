1f831664f62442f225d96d862ac6096cf89089be
JessicaG <61400580+Jessica-star97@users.noreply.github.com>
2022-05-27 22:34:23 -0500

    Update NorthwindController.cs

--- NorthwindClient/Controllers/NorthwindController.cs
+++ NorthwindClient/Controllers/NorthwindController.cs
@@ -28,5 +28,45 @@ public class NorthwindController : ControllerBase
         return NWcontext.Products.ToList<Product>()
         .ToArray();
     }
+    
+    [HttpPost]
+    public IActionResult Create(Category newCategory)
+    {
+        var categoria = _service.Create(newCategory);
+        return CreatedAtAction(nameof(Get), new { id = categoria!.Id }, categoria);
+    }
+
+    [HttpPut("{id}/addcategory")]
+    public IActionResult AddCategory(int id, int CategoryId )
+    {
+        var categoriaToUpdate = _service.Get(id);
+
+        if(categoriaToUpdate is not null)
+        {
+            _service.AddCategory(id,CategoryId );
+            return NoContent();    
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var categoria = _service.Get(id);
+
+        if(categoria is not null)
+        {
+            _service.DeleteByCategory(id);
+            return Ok();
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
 
 }
